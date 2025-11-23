23dcaff528dcea458542035a5e07b4f9afdfd1e7
jcortel85 <>
2025-11-23 07:26:46 +0000

    Membership and Types

---
+++ conditional_statements/membership_and_types/main.py
@@ -0,0 +1,11 @@
+# Product details
+description = "Imported honey, raw and unfiltered"
+price = "5.99"
+count = 120
+
+contains_raw = "raw" in description
+contains_imported = "imported" in description
+
+price_is_float = type(price)
+count_is_int = type(count)
+
