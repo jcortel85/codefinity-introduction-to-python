f6e2c4d983fc9d8dfcc8ccec7ad7371f10cdf7ff
jcortel85 <>
2025-11-23 11:10:55 +0000

    Challenge: Pricing Adjustment Capstone

--- other_data_types/challenge_pricing_adjustment_capstone/main.py
+++ other_data_types/challenge_pricing_adjustment_capstone/main.py
@@ -1,21 +1,27 @@
 grocery_inventory = {
 	"Milk": ("Dairy",3.50,8),
-	"Eggs": ("Dairy",4.50,30),
+	"Eggs": ("Dairy",5.50,30),
 	"Bread": ("Bakery",2.99,15),
 	"Apples": ("Produce",1.50,50)
 	}
 	
-if grocery_inventory["Eggs"][1] >5:
-	print("Eggs are too expensive, reducing the price by $1")
+if grocery_inventory["Eggs"][1] > 5:
+	print("Eggs are too expensive, reducing the price by $1.")
+	(category,price,stock) = grocery_inventory["Eggs"]
+	new_price = price - 1
+	grocery_inventory["Eggs"] = (category, new_price, stock)
 	
 else:
 	print("The price of Eggs is reasonable.")
 
 grocery_inventory["Tomatoes"] = ("Produce",1.20,30)
-print("Iventory after adding Tomatoes", grocery_inventory)
+print("Inventory after adding Tomatoes:", grocery_inventory)
 
 if grocery_inventory["Milk"][2] < 10:
-	print("Milk needs to be restocked. Increasing stock by 20 units")
+	print("Milk needs to be restocked. Increasing stock by 20 units.")
+	(category,price,stock) = grocery_inventory["Milk"]
+	new_stock = stock + 20
+	grocery_inventory["Milk"] = (category, price, new_stock)
 else:
 	print("Milk has sufficient stock.")
 	
